4c956b03189097f1f3bcffdd6fb982ed4ca36c40
Arathi1104 <arathik2711@gmail.com>
2025-11-25 16:24:13 +0530

    Add files via upload

---
+++ mark.php
@@ -0,0 +1,16 @@
+<?php
+$mark=75;
+if($mark>90 and $mark<100){
+print("grade is:S");}
+elseif($mark>80 and $mark<90){
+print("grade is:A");}
+elseif($mark>70 and $mark<80){
+print("grade is:B");}
+elseif($mark>60 and $mark<70){
+print("grade is:C");}
+elseif($mark>50 and $mark<60){
+print("grade is:D");}
+elseif($mark<50){
+print("fail");}
+
+?>
